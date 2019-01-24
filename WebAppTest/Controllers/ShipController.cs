aef52b6beeadf9f212b6bc8d595c27556cd86c44
Richard Clark <richardclark@docman.com>
2019-01-24 15:41:23 +0000

    added admin layout for test

---
+++ WebAppTest/Controllers/ShipController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace WebAppTest.Controllers
+{
+    public class ShipController : Controller
+    {
+        // GET
+        public ActionResult Index()
+        {
+            return View("Index", "_Admin");
+        }
+    }
+}
